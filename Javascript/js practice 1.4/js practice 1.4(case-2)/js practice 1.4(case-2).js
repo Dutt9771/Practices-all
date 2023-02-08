485e9eecec9423a620e97624a799eb6e51e6208e
Dutt Mistry <dutta.mistry@gmail.com>
2023-02-08 18:17:57 +0530

    Practices all

--- Javascript/js practice 1.4/js practice 1.4(case-2)/js practice 1.4(case-2).js
+++ Javascript/js practice 1.4/js practice 1.4(case-2)/js practice 1.4(case-2).js
@@ -1,13 +1,57 @@
 
-var htmlStr= "<html><head></head>body></body></html>";
+let str = "<html><head></head><body><br/></body></html>";
+let str1 = "<html><head></head><body></boy></html>";
+let str2 = "<html><head></head><body><p>this is test element</p></body></html>";
+let str3 = "<html><head></head><body><p>this is test element<br/></p></body></html>";
 
-var lesserTag= htmlStr.indexOf("<");
-var greaterTag=htmlStr.indexOf(">");
-var html = htmlStr.slice(lesserTag+1,greaterTag)
-// console.log(html)
 
-for(var i=0;i<html.length;i++){
-    for(var j=0;j<html.length;i++){
-     if(html.str)   
+
+function validation(str){
+if (str && str.length > 0) {
+    let arr = [];
+    let i = 0;
+
+    while (i < str.length - 1) {
+        if (str[i] === '<') {
+            let j = i + 1;
+            let temp = "";
+            while (j < str.length && str[j] !== ">") {
+                temp += str[j];
+                j++;
+            }
+
+            if(temp[temp.length-1] == "/"){
+                i = j+1;
+                continue;
+            }
+
+            if (temp.startsWith("/") && arr.length>0) {
+                let popedElement = arr.pop();
+                // console.log("Poped = ", popedElement);
+                // console.log("Temp = ", temp.substring(1));
+                if(temp.substring(1) !== popedElement){
+                    console.log("Invalid String");
+                    break;
+                }
+
+            } else {
+                arr.push(temp);
+            }
+            i = j + 1;
+        } else {
+            i++;
+        }
+    }
+    
+
+    if(arr.length == 0){
+        console.log("Valid String");
     }
 }
+}
+
+
+console.log(validation(str));
+console.log(validation(str1))
+console.log(validation(str2))
+console.log(validation(str3))
