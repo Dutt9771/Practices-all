aa0e3008c4d7c6aeecdea3b0649db4eba267543d
Dutt Mistry <dutta.mistry@gmail.com>
2023-02-09 22:48:04 +0530

    Practices all

--- Javascript/js practice 1.5/js practice 1.5(1)/js practice 1.5(1).js
+++ Javascript/js practice 1.5/js practice 1.5(1)/js practice 1.5(1).js
@@ -3,55 +3,69 @@ let str2 = "0000";
 let str3 = "101023";
 
 function validIp(str) {
-    let arr=new Array()
-    let temp="";
-    for(let i=0;i<str.length;i++){
-        if(str[i]=="."){
+
+    let arr = new Array()
+    let temp = "";
+    for (let i = 0; i < str.length; i++) {
+
+        if (str[i] == ".") {
             arr.push(temp)
-            temp="";
-        }else{
-            temp=temp+str[i];
+            temp = "";
+        } else {
+
+            temp = temp + str[i];
         }
     }
+
     arr.push(temp)
-    for(let i=0;i<arr.length;i++){
-        if(arr[i].length>3 || parseInt(arr[i])>255 || parseInt(arr[i]) < 0){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > 3 || parseInt(arr[i]) > 255 || parseInt(arr[i]) < 0) {
             return 0;
         }
-        if(arr[i].length>1 && parseInt(arr[i])==0){
+        if (arr[i].length > 1 && parseInt(arr[i]) == 0) {
             return 0;
         }
-        if(arr[i].length>1 && parseInt(arr[i])!=0 && arr[i][0]=="0"){
+        if (arr[i].length > 1 && parseInt(arr[i]) != 0 && arr[i][0] == "0") {
             return 0
         }
     }
     return 1;
+
 }
 
-function strtoarr(str){
+function strtoarr(str) {
 
-    let len=str.length;
-    if(len>12 || len<4){
+    function containsSpecialChars(str) {
+        const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?~]/;
+        return specialChars.test(str);
+    }
+    let len = str.length;
+    if (len > 12 || len < 4) {
         console.log("IP Address is Invalid")
     }
-    let newstr=str;
-    let newarr=new Array()
+    let newstr = str;
+    let newarr = new Array()
 
-    for(let i=1;i<str.length-2;i++){
-        for(let j=i+1;j<str.length-1;j++){
-            for(let k=j+1;k<str.length;k++){
-                newstr=newstr.substring(0,k)+"."+newstr.substring(k,newstr.length)
-                newstr=newstr.substring(0,j)+"."+newstr.substring(j,newstr.length)
-                newstr=newstr.substring(0,i)+"."+newstr.substring(i,newstr.length);
-                
-                if(validIp(newstr)){
-                    newarr.push(newstr);
-                    return (newstr);
+    for (let i = 1; i < str.length - 2; i++) {
+        for (let j = i + 1; j < str.length - 1; j++) {
+            for (let k = j + 1; k < str.length; k++) {
+                newstr = newstr.substring(0, k) + "." + newstr.substring(k, newstr.length)
+                newstr = newstr.substring(0, j) + "." + newstr.substring(j, newstr.length)
+                newstr = newstr.substring(0, i) + "." + newstr.substring(i, newstr.length);
+                if (containsSpecialChars(str) !== true) {
+                    if (validIp(newstr)) {
+
+                        newarr.push(newstr);
+                        console.log(newstr);
+
+                    }
+                    newstr = str;
+                } else {
+                    return ("IP Address Is Invalid")
                 }
-                newstr=str;
             }
         }
-        
+
     }
 }
 
--- Javascript/js practice 1.5/js practice 1.5(2)/js practice 1.5(2).js
+++ Javascript/js practice 1.5/js practice 1.5(2)/js practice 1.5(2).js
@@ -1,6 +1,34 @@
-let nums = [-1,2,1,-4];
-function target(num) {
-    for(let i=0;i<nums.length;i++){
-        
+let firNums = [-1, 2, 1, -4];
+let secNums = [3, 2, -4, -1];
+
+function target(num, target) {
+    arr = [];
+    for (let i = 0; i < num.length; i++) {
+        for (let j = i + 1; j < num.length; j++) {
+            for (let k = j + 1; k < num.length; k++) {
+                let sum = num[i] + num[j] + num[k];
+                arr.push(sum)
+                console.log(
+                    "sum Of  i = " + i + " ,j = " + j + " ,k = " + k + " th Element" + sum
+                );
+            }
+        }
     }
+
+    console.log(arr);
+    let newarr = arr.map((x) => {
+        return x - target;
+    })
+    console.log(newarr);
+    for (let i = 0; i < newarr.length; i++) {
+        if (-1 <= newarr[i] && newarr[i] <= 1) {
+            console.log("Output Is :", arr[i]);
+        } else {
+            continue;
+        }
+    }
+
 }
+
+target(firNums, 1)
+target(secNums, -4)
