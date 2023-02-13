0e4eeadeb84dcb066845238f243555ec2ca0a24a
Dutt Mistry <dutta.mistry@gmail.com>
2023-02-13 10:26:45 +0530

    Practices all

--- Javascript/js practice 2.2/js practice 2.2(2)/js practice 2.2(2).js
+++ Javascript/js practice 2.2/js practice 2.2(2)/js practice 2.2(2).js
@@ -7,3 +7,9 @@ let BMI=(mass,height)=>{
     return formula;
 }
 console.log(BMI(50,2))
+
+let BMIcalculate=()=>{
+    let mass=document.getElementById("mass").value
+    let height=document.getElementById("height").value
+    document.getElementById("show").innerHTML="Your BMI is "+BMI(mass,height)
+}
