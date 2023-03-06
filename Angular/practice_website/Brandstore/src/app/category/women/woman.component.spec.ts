79114dd480b9510cfe071b9bb6d439fb897e74dd
Dutt Mistry <dutta.mistry@gmail.com>
2023-03-06 14:50:12 +0530

    Practices all

--- Angular/practice_website/Brandstore/src/app/category/women/woman.component.spec.ts
+++ Angular/practice_website/Brandstore/src/app/category/women/woman.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WomanComponent } from './women.component';
+import { WomenComponent } from './women.component';
 
-describe('WomanComponent', () => {
-  let component: WomanComponent;
-  let fixture: ComponentFixture<WomanComponent>;
+describe('womenComponent', () => {
+  let component: WomenComponent;
+  let fixture: ComponentFixture<WomenComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WomanComponent ]
+      declarations: [ WomenComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(WomanComponent);
+    fixture = TestBed.createComponent(WomenComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
